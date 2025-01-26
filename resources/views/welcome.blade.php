6417eec6b991a38adcc03dc28a7105b707ea29ef
sokevinjonas <kjonasdevpro@gmail.com>
2025-01-26 13:49:03 +0000

    ajout des liens vers le store

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -358,16 +358,19 @@
           <p class="lead">Disponible sur Android</p>
           <p class="lead">Bientot disponible sur iOS et PC</p>
           <div class="mt-4">
-            <a href="#" class="btn btn-light btn-lg me-3 mb-3" onclick="confirmDownload(event, '{{ asset('version-app/couturart-v-4.apk') }}')">
-              <i class="fab fa-google-play me-2"></i>Google Play
-              {{-- <i class="fab fa-android me-2"></i>Android --}}
+            <a href="https://play.google.com/store/apps/details?id=bf.couturart.com" target="_blank" class="btn btn-light btn-lg me-3 mb-3">
+              <i class="fab fa-google-play me-2"></i>Playstore
             </a>
-            {{-- <a href="https://app-couturart.eliteero.com" target="_blank" class="btn btn-light btn-lg me-3 mb-3">
+            {{-- <a href="#" class="btn btn-light btn-lg me-3 mb-3" onclick="confirmDownload(event, '{{ asset('version-app/couturart-v-4.apk') }}')">
+              <i class="fab fa-google-play me-2"></i>Google Play
+              <i class="fab fa-android me-2"></i>Android
+            </a> --}}
+            <a href="https://app-couturart.eliteero.com" target="_blank" class="btn btn-light btn-lg me-3 mb-3">
               <i class="fab fa-apple me-2"></i>Iphone
             </a>
             <a href="https://app-couturart.eliteero.com" target="_blank" class="btn btn-light btn-lg mb-3">
               <i class="fas fa-globe me-2"></i>Web & PC
-            </a> --}}
+            </a>
           </div>
         </div>
       </section>
