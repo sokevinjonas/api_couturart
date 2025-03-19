51e0ce8dc373d78284c5b2003161499838072a65
sokevinjonas <kjonasdevpro@gmail.com>
2025-03-19 01:34:11 +0000

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,14 +17,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'nom' => 'Jonas SO',
-            'email' => 'sokevin7@gmail.com',
-            'password' => Hash::make("Mrlarson@1217"),
-            'telephone' => "56785580",
+        // User::factory()->create([
+        //     'nom' => 'Jonas SO',
+        //     'email' => 'sokevin7@gmail.com',
+        //     'password' => Hash::make("Mrlarson@1217"),
+        //     'telephone' => "56785580",
+        //     'pays' => "+226",
+        //     'role' => 'admin'
+        // ]);
+
+         User::factory()->create([
+            'nom' => 'Inoussa GARANE',
+            'email' => 'Inoussagarane0@gmail.com',
+            'password' => Hash::make("primo@#2025"),
+            'telephone' => "54493091",
             'pays' => "+226",
             'role' => 'admin'
         ]);
+
+
 
         $this->call([
             LicenceSeeder::class,
